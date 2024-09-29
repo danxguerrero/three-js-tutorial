df42f6903b3e9547f3e3335a4c34632687bd1ee2
danxguerrero <danxguerrero@yahoo.com>
2024-09-28 23:40:09 -0400

    Add a 3D Torus to the scene with rotation

---
+++ main.js
@@ -0,0 +1,48 @@
+import './style.css'
+
+import * as THREE from 'three'
+
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg')
+})
+
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+camera.position.setZ(30)
+
+renderer.render(scene, camera)
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347, wireframe: true })
+const torus = new THREE.Mesh(geometry, material)
+
+scene.add(torus)
+
+// Add a point light
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(5, 5, 5)
+scene.add(pointLight)
+
+// Add ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50)
+scene.add(lightHelper, gridHelper)
+
+
+const animate = () => {
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+
+  torus.rotation.x += 0.01
+  torus.rotation.y += 0.005
+  torus.rotation.z += 0.1
+}
+
+animate()
