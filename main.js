567c7b819800ba710f8e29533a178a38c4dae5c3
danxguerrero <danxguerrero@yahoo.com>
2024-09-28 23:52:30 -0400

    Add OrbitControls to control camera positioning

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css'
 
 import * as THREE from 'three'
 
+import { OrbitControls } from 'three/examples/jsm/Addons.js'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -35,6 +36,8 @@ const lightHelper = new THREE.PointLightHelper(pointLight)
 const gridHelper = new THREE.GridHelper(200, 50)
 scene.add(lightHelper, gridHelper)
 
+const controls = new OrbitControls(camera, renderer.domElement)
+
 
 const animate = () => {
   requestAnimationFrame(animate)
@@ -43,6 +46,8 @@ const animate = () => {
   torus.rotation.x += 0.01
   torus.rotation.y += 0.005
   torus.rotation.z += 0.1
+
+  controls.update()
 }
 
 animate()
